3545e6a52e0fb032c3b5f44820f5da1808359178
Matt Strimas-Mackey <mes335@cornell.edu>
2025-02-16 07:12:41 -0800

    update species for fig 3

--- code/figure-03_bam-abd.R
+++ code/figure-03_bam-abd.R
@@ -178,7 +178,7 @@ ytick <- 50
 # change breeding biome names to match paper
 biomes <- names(table(spec_coef$breeding_biome))
 biome_names_tidy <- c("Arctic tundra", "Aridland", "Forest",
-      "Grassland", "Habitat generalists", "Wetland & Coast")
+                      "Grassland", "Habitat generalists", "Wetland & Coast")
 
 png(path(figures_dir, "figure-03b_bam-linear-slopes.png"),
     width = 14.4, height = 10, units = "cm", pointsize = 9, res = 600)
@@ -195,14 +195,14 @@ for(i in 1:length(biomes)) {
   if(i == 4) text(x = -0.3, y = 1.1*ylim, label = ylabel, cex = 1.5, xpd = NA, srt = 90)
   if(i > 3) axis(side = 1, at = c(0, 1), labels = c("0", "max"), cex.axis = 1.5, las = 1)
   abline(h = 0, col = alpha("firebrick", 0.42), lwd = 2)
-
+  
   # lines for each species in the biome
   plot_multispec_multiyr(filter(spec_coef, breeding_biome == biomes[i]), no_years = no_yrs)
-
+  
   if(i == 5) text(x = 0.5, y = -1.66*ylim,
                   labels = "Relative abundance within species", xpd = NA, cex = 1.5)
   text(x = -0.03, y = 11/12*ylim, pos = 4, labels = biome_names_tidy[i], cex = 1.5)
-
+  
   # add average biome line
   biome_avg <- biome_coef |> filter(breeding_biome == biomes[i])
   plot_1spec_line_multiyr(biome_avg, col = "black", lwd = 2, no_years = no_yrs)
@@ -225,7 +225,7 @@ cbind(biomes, diff10)
 #########################################################
 ## individual species plots
 
-focal_species <- c("rufhum", "balori")
+focal_species <- c("wilsap", "yebcuc")
 
 species_names <- auk::get_ebird_taxonomy(2022) |> 
   select(species_code, common_name) |>
@@ -235,58 +235,49 @@ for(i in 1:nrow(species_names)){
   
   # collect species data for plots
   this_species_code <- species_names$species_code[i]
-  this_species_name <- gsub("'", "", species_names$common_name[i], fixed = TRUE)
-  this_species_name <- gsub(" ", "", this_species_name, fixed = TRUE)
+  this_species_name <- species_names$common_name[i]
   this_species_coef <- spec_coef |> filter(species_code == this_species_code)
   this_species_trends <- trends |> filter(species_code == this_species_code)
   
-  if(nrow(this_species_coef)==0) {
-    # print out the names of any species without data. 
-    # if all is working well, there should be no species names printed 
-    print(paste("species", this_species_code, this_species_name))
-  } else {
-    
-    # set up plot (to hopefully make pretty for many species)
-    xlimits <- range(this_species_trends$log_abd)
-    la_vals <- seq(-5, 5, by = 1)
-    
-    #  if(xlimits[2] - xlimits[1])
-    #  la_vals <- seq(-6, 6, by = 2)
-    
-    ylimits <- range(this_species_trends$abd_ppy_median)
-    if(ylimits[2]< -10) ylimits[2] <- -10
-    if(ylimits[1]> 10) ylimits[1] <- 10
-    ylimits <- c(-10, 10)
-    
-    
-    formatted_slope <- paste0(ifelse(nu(this_species_coef$est)<0, "n", "p"), "_",
-                              format(round(abs(nu(this_species_coef$est)), digits = 3), nsmall = 3))
-    species_plot_name <- paste0(formatted_slope, "_", this_species_code, "_", this_species_name)
-    
-    plot_loc <- path(figures_dir, paste0("species_scatter_", mod_tag, "_", species_plot_name, ".png"))
-    png(plot_loc, width = 8, height = 8, units = "cm", pointsize = 9, res = 600)
-    par(mar = c(5, 5, 3, 3))
-    
-    # create plot
-    plot(0, 0, col = "white", 
-         xlim = xlimits, 
-         xaxt = "n", xlab = "Relative abundance",
-         ylim = ylimits,
-         yaxt = "n", ylab = "Population trend", 
-         main = this_species_name, 
-         mgp = c(1.5, 0.7, 0))
-    axis(side = 1, at = la_vals, labels = 10^la_vals, tcl = -0.3, mgp = c(1.5, 0.5, 0))
-    axis(side = 2, at = c(-10, 0, 10), las = 1, tcl = -0.3, mgp = c(1.5, 0.7, 0))
-    
-    abline(h = 0, col = alpha("firebrick", 0.2), lwd = 2)
-    
-    points(nu(this_species_trends$log_abd), nu(this_species_trends$abd_ppy_median), 
-           pch = 16, col = alpha("steelblue", 0.3),
-           cex = sqrt(nu(this_species_trends$abd_ppy_sd / mean(this_species_trends$abd_ppy_sd) )))
-    
-    abline(nu(this_species_coef$int_est), nu(this_species_coef$est), col = "steelblue", lwd= 1.5)
-    
-    dev.off()
-  }
+  # set up plot (to hopefully make pretty for many species)
+  xlimits <- range(this_species_trends$log_abd)
+  la_vals <- seq(-5, 5, by = 1)
   
+  #  if(xlimits[2] - xlimits[1])
+  #  la_vals <- seq(-6, 6, by = 2)
+  
+  ylimits <- range(this_species_trends$abd_ppy_median)
+  if(ylimits[2]< -10) ylimits[2] <- -10
+  if(ylimits[1]> 10) ylimits[1] <- 10
+  ylimits <- c(-10, 10)
+  
+  
+  formatted_slope <- paste0(ifelse(nu(this_species_coef$est)<0, "n", "p"), "_",
+                            format(round(abs(nu(this_species_coef$est)), digits = 3), nsmall = 3))
+  
+  plot_loc <- path(figures_dir, paste0("species_scatter_", mod_tag, "_",
+                                       this_species_code, ".png"))
+  png(plot_loc, width = 8, height = 8, units = "cm", pointsize = 9, res = 600)
+  par(mar = c(5, 5, 3, 3))
+  
+  # create plot
+  plot(0, 0, col = "white", 
+       xlim = xlimits, 
+       xaxt = "n", xlab = "Relative abundance",
+       ylim = ylimits,
+       yaxt = "n", ylab = "Population trend", 
+       main = this_species_name, 
+       mgp = c(1.5, 0.7, 0))
+  axis(side = 1, at = la_vals, labels = 10^la_vals, tcl = -0.3, mgp = c(1.5, 0.5, 0))
+  axis(side = 2, at = c(-10, 0, 10), las = 1, tcl = -0.3, mgp = c(1.5, 0.7, 0))
+  
+  abline(h = 0, col = alpha("firebrick", 0.2), lwd = 2)
+  
+  points(nu(this_species_trends$log_abd), nu(this_species_trends$abd_ppy_median), 
+         pch = 16, col = alpha("steelblue", 0.3),
+         cex = sqrt(nu(this_species_trends$abd_ppy_sd / mean(this_species_trends$abd_ppy_sd) )))
+  
+  abline(nu(this_species_coef$int_est), nu(this_species_coef$est), col = "steelblue", lwd= 1.5)
+  
+  dev.off()
 }
